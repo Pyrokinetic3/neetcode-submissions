d7eeb850e63dda9ab1cf4145f0f16d6916e6a674
Pyrokinetic3 <arenkh@icloud.com>
2026-09-18 23:14:41 -0700

    Add: path-crossing - submission-6

---
+++ Data Structures & Algorithms/path-crossing/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        a, b = 0, 0
+        tracker = {(0,0)}
+        for i in path:
+            if i == 'N':
+                a += 1
+            elif i == 'S':
+                a -= 1
+            elif i == 'E':
+                b += 1
+            elif i == 'W':
+                b -= 1
+            loc = (a,b)
+            if loc in tracker:
+                return True
+            tracker.add(loc)
+        return False
+
